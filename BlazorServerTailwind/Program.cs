06cf4953472c0e741dbb3a1a1af534fcfdd2b023
Darren Reid <darren@reidmail.org>
2022-09-16 15:49:39 +1000

    Test

--- BlazorServerTailwind/Program.cs
+++ BlazorServerTailwind/Program.cs
@@ -9,7 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddServerSideBlazor(options =>
+{
+    options.DetailedErrors = true;
+});
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddScoped<LocalStorage>();
 var baseUrl = Environment.GetEnvironmentVariable("DEPLOY_API");
