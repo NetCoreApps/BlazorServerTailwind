896d5c611261eb32c9f7af5c8d81cf5b64fd09f6
Darren Reid <darren@reidmail.org>
2022-09-16 15:23:49 +1000

    Test

--- BlazorServerTailwind/Program.cs
+++ BlazorServerTailwind/Program.cs
@@ -28,11 +28,6 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseWebSockets();
-    app.UseForwardedHeaders(new ForwardedHeadersOptions
-    {
-        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-    });
 }
 
 app.UseHttpsRedirection();
