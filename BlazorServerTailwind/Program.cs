f6114f495ac3f616d348173c841287e95d6034b1
Darren Reid <darren@reidmail.org>
2022-09-16 13:37:42 +1000

    Try explicit `UseWebSockets`.

--- BlazorServerTailwind/Program.cs
+++ BlazorServerTailwind/Program.cs
@@ -27,6 +27,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseWebSockets();
 }
 
 app.UseHttpsRedirection();
