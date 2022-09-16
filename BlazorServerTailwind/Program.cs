c77aa1828fdef3c1db22ace16d9b70560e736818
Darren Reid <darren@reidmail.org>
2022-09-16 13:56:30 +1000

    Testing

--- BlazorServerTailwind/Program.cs
+++ BlazorServerTailwind/Program.cs
@@ -3,6 +3,7 @@ using BlazorServerTailwind.Data;
 using BlazorServerTailwind.Auth;
 using Microsoft.AspNetCore.Components.Authorization;
 using ServiceStack.Blazor;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +29,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
     app.UseWebSockets();
+    app.UseForwardedHeaders(new ForwardedHeadersOptions
+    {
+        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+    });
 }
 
 app.UseHttpsRedirection();
