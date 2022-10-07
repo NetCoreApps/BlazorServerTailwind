099a143fc439b9c14818ced18c6e220ff95b0405
Darren Reid <darren@reidmail.org>
2022-10-07 13:11:08 +1100

    Test failed Migration1001.cs

---
+++ BlazorServerTailwind/Migrations/Migration1001.cs
@@ -0,0 +1,18 @@
+﻿using BlazorServerTailwind.ServiceModel;
+using ServiceStack.OrmLite;
+
+namespace BlazorServerTailwind.Migrations;
+
+/// <summary>
+/// Failed migration test
+/// </summary>
+public class Migration1001 : MigrationBase
+{
+    public override void Up()
+    {
+        Db.Insert(new Hello
+        {
+            Name = "Test failure"
+        });
+    }
+}
